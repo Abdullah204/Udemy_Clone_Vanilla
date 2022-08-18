c0a76e5e3e90c6810c8e17c450c8bb77dbbdbb34
Abdullah Ahmad Fouad <abdullahahmadfouad@gmail.com>
2022-08-18 09:04:52 +0200

    feat : (phase 3) slider and tabs and top categories section

--- script.js
+++ script.js
@@ -1,17 +1,71 @@
 // the function that fetches courses from the server (local) and renders it into the page
-const fetchCourses = async () => {
-  let response = await fetch("http://localhost:3000/courses");
-  if (!response.ok) {
-    throw new Error(`HTTP error: ${response.status}`);
-  }
-  const courses = await response.json();
-  for (let x of courses) {
-    appendCourse(x);
-  }
+let allCourses = [];
+let keys = ["python", "excel", "web", "js", "ds", "aws", "drawing"];
 
+const fetchCourses = async (id, ss) => {
+  ss = ss.toLowerCase();
+
+  let courses = null;
+  for (let key of keys) {
+    // get json file
+    let response = await fetch("http://localhost:3000/" + key);
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    courses = await response.json();
+
+    //reset carousel data
+    document.getElementsByClassName("carousel-inner-" + key)[0].innerHTML = "";
+
+    //fill carousel with items satisfying search bar
+    first = true;
+    let arr = [];
+    for (let x of courses) {
+      arr.push(x);
+
+      newCourse = appendCourseNew(x);
+      courseContainer = document.createElement("div");
+      courseContainer.className += " carousel-item";
+      courseContainer.className += " carousel-item" + key;
+
+      courseContainer.appendChild(newCourse);
+
+      ctitle = x.title.toLowerCase();
+
+      if (id === "start" || id !== key || (id === key && ctitle.includes(ss))) {
+        if (first) {
+          courseContainer.className += " active";
+          first = false;
+        }
+        document
+          .getElementsByClassName("carousel-inner-" + key)[0]
+          .appendChild(courseContainer);
+      }
+    }
+    allCourses[key] = arr;
+    let items = document.querySelectorAll(".carousel .carousel-item" + key);
+
+    items.forEach((el) => {
+      const minPerSlide = Math.min(
+        document.getElementsByClassName("carousel-inner-" + key)[0].childNodes
+          .length,
+        5
+      );
+      let next = el.nextElementSibling;
+      for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+          // wrap carousel by using first child
+          next = items[0];
+        }
+        let cloneChild = next.cloneNode(true);
+        el.appendChild(cloneChild.children[0]);
+        next = next.nextElementSibling;
+      }
+    });
+  }
   return courses;
 };
-fetchCourses();
+fetchCourses("start", "ss");
 
 // create all course elements and add it to the course list
 function appendCourse(course) {
@@ -114,15 +168,88 @@ searchForm.addEventListener("submit", search);
 function search(event) {
   event.preventDefault();
   // get key from input
-  searchString = document.getElementsByClassName("input-field")[0].value;
-  for (let item of courselist) {
-    if (
-      item
-        .getElementsByClassName("course-title")[0]
-        .innerText.toLowerCase()
-        .includes(searchString.toLowerCase())
-    ) {
-      item.className = "course";
-    } else item.className = "hidden-course";
+  ss = document.getElementsByClassName("input-field")[0].value;
+  id = document.getElementsByClassName("tab-pane active")[0].id + "";
+  fetchCourses(id, ss);
+}
+
+function appendCourseNew(course) {
+  //create list item (course)
+  const newli = document.createElement("div");
+  newli.className = "course";
+  // add image
+  const img = document.createElement("img");
+  img.src = course.imgurl;
+  img.alt = "course image";
+  newli.appendChild(img);
+
+  // add title
+  const title = document.createElement("h3");
+  title.className = "course-title";
+  title.innerText = course.title;
+  newli.appendChild(title);
+
+  // add author
+  const instructor = document.createElement("p");
+  instructor.className = "instructor";
+  instructor.innerText = course.author;
+  newli.appendChild(instructor);
+
+  // add rating
+  const ratingcontainer = document.createElement("div");
+  ratingcontainer.className = "rating-container";
+  const rating = document.createElement("span");
+  rating.className = "rating";
+  rating.innerText = course.rating;
+  ratingcontainer.appendChild(rating);
+
+  // adding star icon in rating
+  for (let i = 0; i < Math.floor(course.rating); i++) {
+    const star = document.createElement("i");
+    star.className = "fa-solid fa-star";
+    ratingcontainer.appendChild(star);
   }
+
+  // add half star
+  if (course.rating % 1 > 0.25) {
+    const star = document.createElement("i");
+    star.className = "fa-solid fa-star-half";
+    ratingcontainer.appendChild(star);
+  }
+
+  // number of raters
+  const noOfRaters = document.createElement("span");
+  noOfRaters.className = "raters-number";
+  noOfRaters.innerText = " (" + numberWithCommas(course.raters_no) + ") ";
+  ratingcontainer.appendChild(noOfRaters);
+  // after puting rating , stars and no of raters adding the whole container
+  newli.appendChild(ratingcontainer);
+
+  // adding price container (including old and new price)
+  const pricecontainer = document.createElement("div");
+  pricecontainer.className = "price-container";
+
+  // new price
+  const newPrice = document.createElement("span");
+  newPrice.className = "new-price";
+  newPrice.innerText = "E£" + course.newPrice + " ";
+  pricecontainer.appendChild(newPrice);
+  // old price
+  const oldPrice = document.createElement("span");
+  oldPrice.className = "old-price";
+  oldPrice.innerText = "E£" + course.oldPrice;
+  pricecontainer.appendChild(oldPrice);
+
+  // add pricecontainer
+  newli.appendChild(pricecontainer);
+
+  // check bestseller and mark the course as bestseller if true
+  if (course.isBestSeller === "True") {
+    const bs = document.createElement("span");
+    bs.className = "best-seller";
+    bs.innerText = "Bestseller";
+    newli.appendChild(bs);
+  }
+
+  return newli;
 }
