2786c09fca08e5a5cc3f4bce6510fe917cd644f2
Abdullah Ahmad Fouad <abdullahahmadfouad@gmail.com>
2022-08-23 11:27:48 +0200

    fix : carousel animation and overflow

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// the function that fetches courses from the server (local) and renders it into the page
 let allCourses = [];
 let keys = ["python", "excel", "web", "js", "ds", "aws", "drawing"];
-
+/** @ description the function that fetches courses from the server (local) and renders it into the page
+ * @ param id id of active carousel used to check if we are searching in this carousel or not
+ * @ param ss search string taken from search bar
+ */
 const fetchCourses = async (id, ss) => {
   ss = ss.toLowerCase();
 
@@ -67,112 +69,10 @@ const fetchCourses = async (id, ss) => {
 };
 fetchCourses("start", "ss");
 
-// create all course elements and add it to the course list
-function appendCourse(course) {
-  //create list item (course)
-  const newli = document.createElement("li");
-  newli.className = "course";
-
-  // add image
-  const img = document.createElement("img");
-  img.src = course.imgurl;
-  img.alt = "course image";
-  newli.appendChild(img);
-
-  // add title
-  const title = document.createElement("h3");
-  title.className = "course-title";
-  title.innerText = course.title;
-  newli.appendChild(title);
-
-  // add author
-  const instructor = document.createElement("p");
-  instructor.className = "instructor";
-  instructor.innerText = course.author;
-  newli.appendChild(instructor);
-
-  // add rating
-  const ratingcontainer = document.createElement("div");
-  ratingcontainer.className = "rating-container";
-  const rating = document.createElement("span");
-  rating.className = "rating";
-  rating.innerText = course.rating;
-  ratingcontainer.appendChild(rating);
-
-  // adding star icon in rating
-  for (let i = 0; i < Math.floor(course.rating); i++) {
-    const star = document.createElement("i");
-    star.className = "fa-solid fa-star";
-    ratingcontainer.appendChild(star);
-  }
-
-  // add half star
-  if (course.rating % 1 > 0.25) {
-    const star = document.createElement("i");
-    star.className = "fa-solid fa-star-half";
-    ratingcontainer.appendChild(star);
-  }
-
-  // number of raters
-  const noOfRaters = document.createElement("span");
-  noOfRaters.className = "raters-number";
-  noOfRaters.innerText = " (" + numberWithCommas(course.raters_no) + ") ";
-  ratingcontainer.appendChild(noOfRaters);
-  // after puting rating , stars and no of raters adding the whole container
-  newli.appendChild(ratingcontainer);
-
-  // adding price container (including old and new price)
-  const pricecontainer = document.createElement("div");
-  pricecontainer.className = "price-container";
-
-  // new price
-  const newPrice = document.createElement("span");
-  newPrice.className = "new-price";
-  newPrice.innerText = "E£" + course.newPrice + " ";
-  pricecontainer.appendChild(newPrice);
-  // old price
-  const oldPrice = document.createElement("span");
-  oldPrice.className = "old-price";
-  oldPrice.innerText = "E£" + course.oldPrice;
-  pricecontainer.appendChild(oldPrice);
-
-  // add pricecontainer
-  newli.appendChild(pricecontainer);
-
-  // check bestseller and mark the course as bestseller if true
-  if (course.isBestSeller === "True") {
-    const bs = document.createElement("span");
-    bs.className = "best-seller";
-    bs.innerText = "Bestseller";
-    newli.appendChild(bs);
-  }
-
-  // add item (course) to course list
-  const list = document.getElementsByClassName("course-list")[0];
-  list.appendChild(newli);
-}
-
-// used to add thousand comma to number of raters e.g. 1234 --> 1,234
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-// variable containing list items of course list (list of courses)
-const courselist = document.getElementsByClassName("course-list")[0].children;
-
-// get search form and link its submit to search function
-const searchForm = document.getElementsByClassName("search-form")[0];
-searchForm.addEventListener("submit", search);
-
-// search course titles including search key as substring (and hiding ones not including)
-function search(event) {
-  event.preventDefault();
-  // get key from input
-  ss = document.getElementsByClassName("input-field")[0].value;
-  id = document.getElementsByClassName("tab-pane active")[0].id + "";
-  fetchCourses(id, ss);
-}
-
+/** @ description the function that fetches courses from the server (local) and renders it into the page
+ * @ param id id of active carousel used to check if we are searching in this carousel or not
+ * @ param ss search string taken from search bar
+ */
 function appendCourseNew(course) {
   //create list item (course)
   const newli = document.createElement("div");
@@ -253,3 +153,33 @@ function appendCourseNew(course) {
 
   return newli;
 }
+
+function numberWithCommas(x) {
+  x = x.toString();
+  var pattern = /(-?\d+)(\d{3})/;
+  while (pattern.test(x)) x = x.replace(pattern, "$1,$2");
+  return x;
+}
+
+let test = document.createElement("div");
+test.innerHTML = "<div class = 'div1'>div1 <div> div2</div></div>";
+document.getElementsByTagName("body")[0].appendChild(test);
+
+/**<div class="course">
+ * <img src="imgs/python5.jpeg" alt="course image">
+ * <h3 class="course-title">Python Beyond the Basics - Object-Oriented Programming</h3>
+ * <p class="instructor">Infinite Skills</p>
+ * <div class="rating-container">
+ * <span class="rating">4.2</span>
+ * <i class="fa-solid fa-star"></i>
+ * <i class="fa-solid fa-star"></i>
+ * <i class="fa-solid fa-star"></i>
+ * <i class="fa-solid fa-star"></i>
+ * <span class="raters-number"> (1,284) </span>
+ * </div>
+ * <div class="price-container">
+ * <span class="new-price">E£199.99 </span>
+ * <span class="old-price">E£679.99</span>
+ * </div>
+ * <span class="best-seller">Bestseller</span>
+ * </div> */
